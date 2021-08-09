acc56ff42f8c371954950a79d435c794ae827593
Harikrishna Patnala <harikrishna.patnala@gmail.com>
2021-08-09 09:18:11 +0530

    Addressed review comments

--- engine/orchestration/src/main/java/com/cloud/vm/VirtualMachineManagerImpl.java
+++ engine/orchestration/src/main/java/com/cloud/vm/VirtualMachineManagerImpl.java
@@ -3742,7 +3742,7 @@ public class VirtualMachineManagerImpl extends ManagerBase implements VirtualMac
 
         final ServiceOfferingVO currentServiceOffering = _offeringDao.findByIdIncludingRemoved(vmInstance.getId(), vmInstance.getServiceOfferingId());
         final DiskOfferingVO currentDiskOffering = _diskOfferingDao.findByIdIncludingRemoved(currentServiceOffering.getDiskOfferingId());
-        final DiskOfferingVO newDiskOffering = _diskOfferingDao.findByIdIncludingRemoved(newServiceOffering.getDiskOfferingId());
+        final DiskOfferingVO newDiskOffering = _diskOfferingDao.findById(newServiceOffering.getDiskOfferingId());
 
         checkIfNewOfferingStorageScopeMatchesStoragePool(vmInstance, newDiskOffering);
 
